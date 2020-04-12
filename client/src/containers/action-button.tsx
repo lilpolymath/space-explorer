1a4c317d0f854ce3c34190cf1283a44d0c3d1898
lilpolymath <ayofavour7@gmail.com>
2020-04-12 17:37:34 +0100

    feat: action button component.

--- client/src/containers/action-button.tsx
+++ client/src/containers/action-button.tsx
@@ -1,7 +1,66 @@
 import React from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
-const ActionButton: React.FC<any> = () => {
-  return <div/>;
-}
+import { GET_LAUNCH_DETAILS } from '../pages/launch';
+import Button from '../components/button';
+import * as LaunchDetailTypes from '../pages/__generated__/LaunchDetails';
+
+const TOGGLE_CART = gql`
+  mutation addOrRemoveFromCart($launchId: ID!) {
+    addOrRemoveFromCart(id: $launchId) @client
+  }
+`;
+
+export const CANCEL_TRIP = gql`
+  mutation cancel($launchId: ID!) {
+    cancelTrip(id: $launchId) {
+      success
+      message
+      launch {
+        id
+        isBooked
+      }
+    }
+  }
+`;
+
+interface ActionButtonProps
+  extends Partial<LaunchDetailTypes.LaunchDetails_launch> { }
+
+const ActionButton: React.FC<ActionButtonProps> = ({
+  isBooked,
+  id,
+  isInCart,
+}) => {
+  const [mutate, { loading, error }] = useMutation(
+    isBooked ? CANCEL_TRIP : TOGGLE_CART,
+    {
+      variables: {
+        launchId: id,
+      },
+      refetchQueries: [
+        {
+          query: GET_LAUNCH_DETAILS,
+          variables: { launchId: id },
+        },
+      ],
+    }
+  );
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>An Error Occured.</p>;
+  return (
+    <div>
+      <Button onClick={() => mutate()} data-test-id={'action-button'}>
+        {isBooked
+          ? 'Cancel this trip'
+          : isInCart
+            ? 'Remove From Cart'
+            : 'Add to Cart'}
+      </Button>
+    </div>
+  );
+};
 
 export default ActionButton;
